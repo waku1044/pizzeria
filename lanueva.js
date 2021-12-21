adf391e89fb145b17b5479a683fe1a91f4d88fb2
waku1044 <75543829+waku1044@users.noreply.github.com>
2021-12-21 09:35:28 -0300

    Add files via upload

--- lanueva.js
+++ lanueva.js
@@ -4,14 +4,27 @@ $(function(){
 	});
 	$('#pizzas').click(function(){
 		$('.pizza1, .pizza2, .pizza3').slideToggle(500);
+		$('.empa1, .empa2, .empa3').hide();
+		$('.bebida1,.bebida2,.bebida3').hide();
+		$('.postre1, .postre2, .postre3').hide();
+
 	});
 	$('#empanadas').click(function(){
 		$('.empa1, .empa2, .empa3').slideToggle(500);
+		$('.pizza1, .pizza2, .pizza3').hide();
+		$('.bebida1,.bebida2,.bebida3').hide();
+		$('.postre1, .postre2, .postre3').hide();
 	});
 	$('#bebidas').click(function(){
 		$('.bebida1,.bebida2,.bebida3').slideToggle(500);
+		$('.pizza1, .pizza2, .pizza3').hide();
+		$('.empa1, .empa2, .empa3').hide();
+		$('.postre1, .postre2, .postre3').hide();
 	});
 	$('#postres').click(function(){
 		$('.postre1, .postre2, .postre3').slideToggle(500);
+		$('.pizza1, .pizza2, .pizza3').hide();
+		$('.empa1, .empa2, .empa3').hide();
+		$('.bebida1,.bebida2,.bebida3').hide();
 	});
 });
