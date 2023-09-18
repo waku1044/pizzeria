872f9ca5998b314c714a06fd731c3fb0e9402e39
Walter Gimenez <75543829+waku1044@users.noreply.github.com>
2023-09-18 00:03:20 -0300

    se organizaron los archivos y se empezo a crear el header y el navbar

--- lanueva.js
+++ lanueva.js
@@ -1,30 +1,30 @@
-$(function(){
-	$('.btn-menu').click(function(){
-		$('.submenu').slideToggle(500);
-	});
-	$('#pizzas').click(function(){
-		$('.pizza1, .pizza2, .pizza3').slideToggle(500);
-		$('.empa1, .empa2, .empa3').hide();
-		$('.bebida1,.bebida2,.bebida3').hide();
-		$('.postre1, .postre2, .postre3').hide();
+// $(function(){
+// 	$('.btn-menu').click(function(){
+// 		$('.submenu').slideToggle(500);
+// 	});
+// 	$('#pizzas').click(function(){
+// 		$('.pizza1, .pizza2, .pizza3').slideToggle(500);
+// 		$('.empa1, .empa2, .empa3').hide();
+// 		$('.bebida1,.bebida2,.bebida3').hide();
+// 		$('.postre1, .postre2, .postre3').hide();
 
-	});
-	$('#empanadas').click(function(){
-		$('.empa1, .empa2, .empa3').slideToggle(500);
-		$('.pizza1, .pizza2, .pizza3').hide();
-		$('.bebida1,.bebida2,.bebida3').hide();
-		$('.postre1, .postre2, .postre3').hide();
-	});
-	$('#bebidas').click(function(){
-		$('.bebida1,.bebida2,.bebida3').slideToggle(500);
-		$('.pizza1, .pizza2, .pizza3').hide();
-		$('.empa1, .empa2, .empa3').hide();
-		$('.postre1, .postre2, .postre3').hide();
-	});
-	$('#postres').click(function(){
-		$('.postre1, .postre2, .postre3').slideToggle(500);
-		$('.pizza1, .pizza2, .pizza3').hide();
-		$('.empa1, .empa2, .empa3').hide();
-		$('.bebida1,.bebida2,.bebida3').hide();
-	});
-});
+// 	});
+// 	$('#empanadas').click(function(){
+// 		$('.empa1, .empa2, .empa3').slideToggle(500);
+// 		$('.pizza1, .pizza2, .pizza3').hide();
+// 		$('.bebida1,.bebida2,.bebida3').hide();
+// 		$('.postre1, .postre2, .postre3').hide();
+// 	});
+// 	$('#bebidas').click(function(){
+// 		$('.bebida1,.bebida2,.bebida3').slideToggle(500);
+// 		$('.pizza1, .pizza2, .pizza3').hide();
+// 		$('.empa1, .empa2, .empa3').hide();
+// 		$('.postre1, .postre2, .postre3').hide();
+// 	});
+// 	$('#postres').click(function(){
+// 		$('.postre1, .postre2, .postre3').slideToggle(500);
+// 		$('.pizza1, .pizza2, .pizza3').hide();
+// 		$('.empa1, .empa2, .empa3').hide();
+// 		$('.bebida1,.bebida2,.bebida3').hide();
+// 	});
+// });
